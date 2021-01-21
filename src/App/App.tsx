71a777f369a409ad3d985f884bbb309bc7e6eb3c
goncy <gonzalo.pozzo4@gmail.com>
2021-01-21 17:32:16 -0300

    Update alt

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
     <div className={styles.container}>
       <header className={styles.header}>
         <h1>
-          <img alt="logo" className={styles.logo} src="/logo.svg" />
+          <img alt="TradeHelm" className={styles.logo} src="/logo.svg" />
         </h1>
         <h3 className={styles.text}>Lets get this party started</h3>
       </header>
