96986dd89b3576fd31f8a5cf6ee5733b0f76acc0
goncy <gonzalo.pozzo4@gmail.com>
2021-01-21 17:33:01 -0300

    Use better elements

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,14 +4,14 @@ import styles from "./App.module.scss";
 
 const App: React.FC = () => {
   return (
-    <div className={styles.container}>
+    <main className={styles.container}>
       <header className={styles.header}>
         <h1>
           <img alt="TradeHelm" src="/logo.svg" />
         </h1>
         <h3>Lets get this party started</h3>
       </header>
-    </div>
+    </main>
   );
 };
 
